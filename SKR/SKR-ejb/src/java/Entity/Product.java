b3a33fff22d90725e5589f1c65d1fbc6c0f96539
ItRealRew <>
2018-03-20 18:47:27 +0300

    Добавленны вроде бы все классы модели. Хранилище, продукт.

---
+++ SKR/SKR-ejb/src/java/Entity/Product.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Entity;
+
+/**
+ *
+ * @author user
+ */
+public class Product {
+    public int id;
+    public String NameProduct;
+    public int lot;
+    public String Typelot;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNameProduct() {
+        return NameProduct;
+    }
+
+    public void setNameProduct(String NameProduct) {
+        this.NameProduct = NameProduct;
+    }
+
+    public int getLot() {
+        return lot;
+    }
+
+    public void setLot(int lot) {
+        this.lot = lot;
+    }
+
+    public String getTypelot() {
+        return Typelot;
+    }
+
+    public void setTypelot(String Typelot) {
+        this.Typelot = Typelot;
+    }
+
+    public Product(int id, String NameProduct, int lot, String Typelot) {
+        this.id = id;
+        this.NameProduct = NameProduct;
+        this.lot = lot;
+        this.Typelot = Typelot;
+    }
+}
---
+++ SKR/SKR-ejb/src/java/Entity/Storage.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Entity;
+
+/**
+ *
+ * @author user
+ */
+public class Storage {
+    public int id;
+    public String name;
+
+    public Storage(int id, String name, int idUser, String type) {
+        this.id = id;
+        this.name = name;
+        this.idUser = idUser;
+        this.type = type;
+    }
+    public int idUser;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getIdUser() {
+        return idUser;
+    }
+
+    public void setIdUser(int idUser) {
+        this.idUser = idUser;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+    public String type;
+}
